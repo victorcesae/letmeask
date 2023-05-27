ce261468fa2c12f7600c16464f3c4cf38938076d
Victor Evangelista <victor.evangelista2010@gmail.com>
2023-05-27 18:06:26 -0300

    fix: change project to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import { Home } from "./Pages/Home";
+import { NewRoom } from "./Pages/NewRoom";
+import { Room } from "./Pages/Room";
 
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import { Home } from './Pages/Home';
-import { NewRoom } from './Pages/NewRoom';
-import { Room } from './Pages/Room';
-
-import { AuthContextProvider } from './contexts/AuthContext';
+import { AuthContextProvider } from "./contexts/AuthContext";
 import NotFound from "./Pages/NotFound";
 import { AdminRoom } from "./Pages/AdminRoom";
 
-
 function App() {
-
-  
   return (
     <Router>
       <AuthContextProvider>
@@ -19,12 +20,12 @@ function App() {
           <Route index element={<Home />} />
           <Route path="403" element={<NotFound />} />
           <Route path="rooms">
-            <Route index element={<Navigate to='/rooms/new' replace/>} />
+            <Route index element={<Navigate to="/rooms/new" replace />} />
             <Route path="new" element={<NewRoom />} />
             <Route path=":id" element={<Room />} />
             <Route path="admin/:id" element={<AdminRoom />} />
-            </Route>
-        </ Routes>
+          </Route>
+        </Routes>
       </AuthContextProvider>
     </Router>
   );
