03314a008c85bddc46580a1413a0b9f89fc10103
Victor Evangelista <victor.evangelista2010@gmail.com>
2023-05-27 20:41:26 -0300

    fix: deploy vite

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import "./services/firebase";
 
 import "./styles/global.scss";
-import React from "react";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <App />
 );
